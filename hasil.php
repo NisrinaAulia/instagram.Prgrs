e366def39aa6c7e3fa39c53f8b50e7102f1c89ba
NisrinaAulia <31603046+NisrinaAulia@users.noreply.github.com>
2017-10-22 22:45:36 +0700

    Form_Instagram_Progres
    Nisrina Aulia Sholehah (26)
    XI RPL 2
    SMK TELKOM PURWOKERTO

---
+++ hasil.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+
+$host= "localhost";
+$user= "root";
+$password="";
+$email=$_POST['email'];
+$fullname=$_POST['fullname'];
+$username=$_POST['username'];
+$pw=$_POST['pw'];
+echo "<center>CONGRATULATION</center>";
+echo "<center><tr><td>email</td><td>:</td><td>$email</td></tr><br>";
+echo "<tr><td>fullname</td><td>:</td><td>$fullname</td></tr><br>";
+echo "<tr><td>username</td><td>:</td><td>$username</td></tr><br>";
+echo "<tr><td>password</td><td>:</td><td>$pw</td></tr></center>";
+?>	
+</body>
+</html>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<font face="arial">
+
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div id="login">
+	<center><h1>Instagram</h1></center>
+	<div class="kata"> sign up to see photos and videos<br>from your friends</div>
+				<fieldset>
+
+			<button><a href="http://facebook.com">Login With Facebook</a></button>
+			<p>OR</p>
+				<form action="hasil.php" method="POST">
+				<font face="billabong">
+					<input type="text" name="email" placeholder="Mobile Number Or Email">
+					<input type="text" name="fullname" placeholder="Full Name">
+					<input type="text" name="username" placeholder="Username"/><br>
+					<input type="password" name="pw" placeholder="password">
+					<input type="submit" name="submit" value="Sign Up">
+					<div class="border-p"></div>
+				</form>
+			</fieldset>
+		</div>	
+</body>
+</html>
